e18232ebaa944c1373ed9084a984c2fe6ba82711
Daniel Hung <daniel@fractory.com>
2020-07-30 19:57:53 +0100

    database model  creation and validation

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from password_strength import PasswordPolicy
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Added as suggested by pytest warnings
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 
+def init_db():
+    """Wrapper function to initialise the database, necessary for testing"""
+    db.create_all()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Wrapper funtion to register the CLI initdb command for the FLASK app"""
+    init_db()
+    print('Initialized the database.')
+
 class User(db.Model):
+    """Main database User model using the SQLAlchemy syntax with fields """
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(128))
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def set_hash_password(self, password):
+        """sets the user model password_hash based on password encryption"""
+        self.password_hash = pbkdf2_sha256.hash(password)
+
+    def authentificate(self, password):
+        """check that the encrypted password matches the User stored password_hash"""
+        return pbkdf2_sha256.verify(password, self.password_hash)
+
+    def check_password(self, password):
+        """check the password strength"""
+        """ With length of at least 8 characters, 1 upper case, 1number, and 1 special character"""
+        policy = PasswordPolicy.from_names(length=8,uppercase=1,numbers=1,special=1)
+        return len(policy.test(password)) == 0
--- test_app.py
+++ test_app.py
@@ -1,6 +1,34 @@
+import os
 import pytest
+import tempfile
 
 import app
 
 def test_db_table():
     assert "users" in app.db.metadata.tables.keys()
+
+def test_db_fields():
+    field_set= set(["id","username","email","password_hash","date_created"])
+    db_field_set = set(app.db.metadata.tables["users"].columns.keys())
+    assert len(db_field_set.intersection(field_set)) == len(field_set)
+
+def test_create_user():
+    user = app.User(username='peter', email='peter@example.org')
+    user.set_hash_password("Passw0rd!")
+    app.db.session.add(user)
+    app.db.session.commit()
+    assert len(app.User.query.filter_by(username='peter').filter_by(email='peter@example.org').all()) == 1
+
+def test_user_authentificate():
+    user=app.User.query.filter_by(username='peter').filter_by(email='peter@example.org').first()
+    assert user.authentificate("Passw0rd!")
+
+def test_user_check_password():
+    user=app.User.query.filter_by(username='peter').filter_by(email='peter@example.org').first()
+    assert user.check_password("Passw0rd!")
+
+def test_delete_user():
+    user=app.User.query.filter_by(username='peter').filter_by(email='peter@example.org').first()
+    app.db.session.delete(user)
+    app.db.session.commit()
+    assert len(app.User.query.filter_by(username='peter').filter_by(email='peter@example.org').all()) == 0
